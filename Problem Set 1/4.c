ff6b3abe0ea756ecde9b7ef9ece9da3ec12e95c8
163432591 <73006032+163432591@users.noreply.github.com>
2020-10-18 20:42:26 +0600

    Add files via upload

---
+++ Problem Set 1/4.c
@@ -0,0 +1,16 @@
+/*
+Print entire array each element in newline.
+Name:Rafeul Anam Udoy
+Id:163432591
+*/
+
+#include <stdio.h>
+int main()
+{
+    int array[100] = {1, 2, 3, 4, 5, 6, 7,8, 9, 10},n=10,i;
+    for(i=0;i<n;i++)
+    {
+        printf("%d\n",array[i]);
+    }
+    return 0;
+}
