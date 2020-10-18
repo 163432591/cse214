47094d4a017a0f52b64d4db731fc59dadf2441d7
163432591 <73006032+163432591@users.noreply.github.com>
2020-10-18 20:41:38 +0600

    Add files via upload

---
+++ Problem Set 1/2.c
@@ -0,0 +1,22 @@
+/*
+Problem No 2:Scan values from user into an array until end of file
+Name:Rafeul Anam Udoy
+Id:163432591
+*/
+
+#include<stdio.h>
+int main()
+{
+    int array[100];
+    int i = 0;
+    while(scanf("%d",&array[i]) != EOF)
+    {
+        i++;
+        if(i+1>100)
+        {
+            printf("Overflow");
+            break;
+        }
+    }
+    return 0;
+}
