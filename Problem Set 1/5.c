e8202bc13ec397e12005350f62c8d866ee451439
163432591 <73006032+163432591@users.noreply.github.com>
2020-10-18 20:42:49 +0600

    Add files via upload

---
+++ Problem Set 1/5.c
@@ -0,0 +1,16 @@
+/*
+Problem no 5:Print entire array each element separated by space
+Name:Rafeul Anam Udoy
+Id:163432591
+*/
+
+#include <stdio.h>
+int main()
+{
+    int array[100] = {1, 2, 3, 4, 5, 6, 7,8, 9, 10},n=10,i;
+    for(i=0;i<n;i++)
+    {
+        printf("%d ",array[i]);
+    }
+    return 0;
+}
