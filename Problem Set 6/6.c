aacd871823457735f32fd5fd8f560f79eab1631b
163432591 <73006032+163432591@users.noreply.github.com>
2020-12-22 20:36:36 +0600

    Add files via upload

---
+++ Problem Set 6/6.c
@@ -0,0 +1,66 @@
+/*
+Define a function to display the complete Linked List in reverse order with
+a recursive function using problem 4.
+Name:Rafeul Anam Udoy
+Id:163432591
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+
+int main()
+{
+    struct Node *head;
+    struct Node *second;
+    struct Node *third;
+
+
+
+    head = (struct Node *)malloc(sizeof(struct Node));
+    second =(struct Node *)malloc(sizeof(struct Node));
+    third = (struct Node *)malloc(sizeof(struct Node));
+
+
+
+    head->data = 7;
+    head->next = second;
+
+
+    second->data = 11;
+    second->next = third;
+
+
+    third->data = 41;
+    third->next = NULL;
+
+    display(head);
+    return 0;
+}
+void display(struct Node *ptr)
+{
+    if(ptr==NULL)
+    {
+        printf("Empty List\n");
+        return;
+    }
+
+    else
+    {
+
+        if(ptr==NULL)
+        {
+            return;
+        }
+        display(ptr->next);
+        printf("%d ",ptr->data);
+    }
+    printf("\n");
+}
