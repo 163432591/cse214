d13ff9187fcec79278f612a6d034d6c67cb954b0
163432591 <73006032+163432591@users.noreply.github.com>
2020-12-23 14:23:00 +0600

    Add files via upload

---
+++ Problem Set 7/1.c
@@ -0,0 +1,65 @@
+/*
+Define a “Create” function to declare a Linked List with four node using dynamic
+memory allocation. Print the complete Linked List after calling create function.
+
+Name:Rafeul Anam Udoy
+Id:163432591
+
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+struct Node* create_list();
+void print_list(struct Node *ptr);
+
+int main()
+{
+    struct Node *head = NULL;
+
+    head = create_list();
+    print_list(head);
+
+    return 0;
+}
+
+struct Node* create_list()
+{
+   struct Node *a, *b, *c, *d;
+
+    a = (struct Node *) malloc(sizeof(struct Node));
+    b = (struct Node *) malloc(sizeof(struct Node));
+    c = (struct Node *) malloc(sizeof(struct Node));
+    d = (struct Node *) malloc(sizeof(struct Node));
+
+    a->data = 10;
+    a->next = b;
+
+    b->data = 20;
+    b->next = c;
+
+    c->data = 30;
+    c->next = d;
+
+    d->data= 40;
+    d->next = NULL;
+
+    return a;
+}
+
+void print_list(struct Node *ptr){
+    while(ptr!=NULL){
+        printf("%d ",ptr->data);
+        ptr = ptr->next;
+    }
+    printf("\n");
+}
