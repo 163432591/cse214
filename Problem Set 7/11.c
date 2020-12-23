8fb4ab41cf0572bd6a899712339c5418ff1e6bc8
163432591 <73006032+163432591@users.noreply.github.com>
2020-12-23 14:27:36 +0600

    Add files via upload

---
+++ Problem Set 7/11.c
@@ -0,0 +1,87 @@
+/*
+Copy a Linked List into another Linked List. Display both List
+before and after copy. (Don’t use recursion).
+Name:Rafeul Anam Udoy
+Id:163432591
+
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+ struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+struct Node* create_list();
+void print_list(struct Node *temp);
+struct Node* copy_list(struct Node *list);
+
+int main()
+{
+    struct Node *head = NULL;
+    struct Node *list = NULL;
+
+    head = create_list();
+    print_list(head);
+
+    list = copy_list(head);
+    print_list(list);
+}
+struct Node* create_list()
+{
+   struct  Node *a, *b, *c, *d;
+
+    a = (struct Node *) malloc(sizeof(struct Node));
+    b = (struct Node *) malloc(sizeof(struct Node));
+    c = (struct Node *) malloc(sizeof(struct Node));
+    d = (struct Node *) malloc(sizeof(struct Node));
+
+    a->data = 10;
+    a->next = b;
+
+    b->data= 20;
+    b->next = c;
+
+    c->data = 30;
+    c->next = d;
+
+    d->data = 40;
+    d->next = NULL;
+
+    return a;
+}
+
+void print_list(struct Node *temp){
+    while(temp!=NULL){
+        printf("%d ",temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+
+struct Node* copy_list(struct Node *temp)
+{
+    struct Node *head2;
+    struct Node *temp2;
+
+    temp2 = (struct Node*) malloc(sizeof (struct Node));
+
+    temp2->data = temp->data;
+    temp2->next = temp->next;
+
+    head2 = temp2;
+    temp = temp->next;
+    while(temp)
+    {
+        temp2 = (struct Node*) malloc(sizeof (struct Node) ) ;
+        temp2->data = temp->data;
+        temp2->next = temp->next;
+        temp = temp->next;
+    }
+    return head2;
+}
+
