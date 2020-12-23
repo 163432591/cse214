d7c42b4bc6eab44efa653e5bfa977d2885106e93
163432591 <73006032+163432591@users.noreply.github.com>
2020-12-23 14:24:27 +0600

    Add files via upload

---
+++ Problem Set 7/4.c
@@ -0,0 +1,57 @@
+/*
+Define a “Front” function to display the first element of the Linked List.
+Name:Rafeul Anam Udoy
+Id:163432591
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+ struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+struct Node* create_list();
+void empty_list(struct Node *ptr);
+void print_list(struct Node *ptr);
+
+int main()
+{
+    struct Node *head = NULL;
+
+    head = create_list();
+    printf("%d\n", front(head));
+
+    return 0;
+}
+struct Node* create_list()
+{
+    struct Node *a, *b, *c, *d;
+
+    a = (struct Node *) malloc(sizeof(struct Node));
+    b = (struct Node *) malloc(sizeof(struct Node));
+    c = (struct Node *) malloc(sizeof(struct Node));
+    d = (struct Node *) malloc(sizeof(struct Node));
+
+    a->data = 10;
+    a->next = b;
+
+    b->data = 20;
+    b->next = c;
+
+    c->data = 30;
+    c->next = d;
+
+    d->data = 40;
+    d->next = NULL;
+
+    return a;
+}
+
+int front(struct Node *ptr){
+    if(ptr!=NULL){
+        return ptr->data;
+    }
+}
